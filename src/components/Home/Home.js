c435eca2ac4f1af6fdf10c12d02496a6c49f686b
RituKotadiya <kotadiyaritu@gmail.com>
2025-07-09 13:09:48 +0530

    commit on 09/07/2025

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import "./Home.scss"
-import image from './simple.jpg';
-import im from './old.webp';
+import image from './dark.jpg';
+import im from './simple.jpg';
+
 
 function Home() {
       return (
@@ -8,17 +9,28 @@ function Home() {
                   <div className=".container-fluid image-container">
                         <img src={image} alt="bag-ground" style={{ width: "100%" }} />
                         <h1 className="overlay-text">Your One Stop <br />Shop for Exerything</h1>
+                        <p className="overlay-txt">Find everything you need in one place - great deals, top quality, and endless convenience. Shop anytime, anywhere. Instant deals!</p>
 
                         <div className="overlay-card" style={{ width: "18rem" }}>
                               <img src={im} className="card-img-top" alt="Card image" style={{ borderRadius: "120px 120px 0 0", height: "250px", width: "200px" }} />
 
                               <img src={im} className="card-img-top" alt="Card image" style={{ borderRadius: "120px 120px 0 0", height: "250px", width: "200px" }} />
-                              {/* <div className="card-body">
-                              <p className="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                              </p>
-                        </div> */}
+
+                              <img src={im} className="card-img-top" alt="Card image" style={{ borderRadius: "120px 120px 0 0", height: "250px", width: "200px" }} />
+
+                              <img src={im} className="card-img-top" alt="Card image" style={{ borderRadius: "120px 120px 0 0", height: "250px", width: "200px" }} />
+
+                              <img src={im} className="card-img-top" alt="Card image" style={{ borderRadius: "120px 120px 0 0", height: "250px", width: "200px" }} />
+
+                              <img src={im} className="card-img-top" alt="Card image" style={{ borderRadius: "120px 120px 0 0", height: "250px", width: "200px" }} />
+
                         </div>
+                        <div className="text">
+                              <h1>New in</h1>
+                              <p>Bringing you the Newest Trends, Latest Styles, and Must-Have Collections -- Just Dropped!</p>
+                        </div>
+                       
+
                   </div>
 
 
